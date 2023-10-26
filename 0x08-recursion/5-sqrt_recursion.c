6d5bbc3116d656c0ad19a1ba3e960ae1fe5d4e76
LydiaMasha <lydiamacharia19@gmail.com>
2023-10-26 04:24:33 -0700

    Square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,8 +13,12 @@ int _sqrt_recursion(int n)
 		return (-1);
 	}
 	else
-		if (n != 0)
+		if (n ==  0 || n == 1)
 		{
-			return (_sqrt_recursion(n));
+			return (n);
+		}
+		else
+		{
+			return (_sqrt_recursion(n, 0, n));
 		}
 }
