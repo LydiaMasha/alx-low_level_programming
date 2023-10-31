d69ea0624b88eee6ea42421ed42eae8218ba846b
LydiaMasha <lydiamacharia19@gmail.com>
2023-10-31 10:57:06 -0700

    Duplicated pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer but to a new allocated space(copy of original)
+ * @str: char
+ * @z: string to copy from
+ *
+ * Return: pointer to duplicated string, NULL if fails
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int a = 0; 
+	int z = 0;
+
+		if (str == NULL)
+	{
+		return (NULL);
+	}
+	s = malloc(sizeof(char) * (a + 1));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	while (str[a] != '\0')
+		a++;
+
+	for (z = 0; str[z]; z++)
+		s[z] = str[z];
+
+	return (s);
+}
