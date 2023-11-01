ed45812eb7e5727c53bb75675a6f9995eae6dc22
LydiaMasha <lydiamacharia19@gmail.com>
2023-11-01 10:04:06 -0700

    Name of program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,11 +9,11 @@
  * Return: Always 0 (succss)
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int a;
 
-	printf("Name of program is \"%s\".\n", argv[0]);
+	printf("Name of program is: %s\n", argv[0]);
 	for (a = 0; a < argc; a++)
 	{
 		printf("argv[%d] = %s\n", a, argv[a]);
