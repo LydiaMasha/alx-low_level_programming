aa5bca2417f33672881b12c917de02067fc90991
LydiaMasha <lydiamacharia19@gmail.com>
2023-11-07 11:56:25 -0800

    Requirements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,9 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
