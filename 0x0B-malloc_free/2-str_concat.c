52543ff821a08f7faeb2e68858f49b8aeacbbe88
LydiaMasha <lydiamacharia19@gmail.com>
2023-11-19 23:41:46 -0800

    Concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,39 +13,34 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *link;
-	int b = 0;
-	int ti = 0;
+	int len_s1 = 0;
+	int len_s2 = 0;
+	int i;
+	int j;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len_s1] != '\0')
+		len_s1++;
+	while (s2[len_s2] != '\0')
+		len_s2++;
+
+	link = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+
+	if (link == NULL)
 	{
-		return ("");
+		return (NULL);
 	}
 
-	b = ti = 0;
+	for (i = 0; i < len_s1; i++)
+		link[i] = s1[i];
+	for (j = 0; j < len_s2; j++)
+		link[i + j] = s2[j];
 
-	while (s1[b] != '\0')
-		b++;
-	while (s2[ti] != '\0')
-		ti++;
+	link[i + j] = '\0';
 
-	link = malloc(sizeof(char) * (b + ti + 1));
-			if (link == NULL)
-			{
-			return (NULL);
-			}
-			b = ti = 0;
-			while (s1[b] != '\0')
-			{
-				link[b] = s1[b];
-			b++;
-			}
-			while (s2[ti] != '\0')
-			{
-				link[ti] = s2[ti];
-			ti++;
-			b++;
-			}
-			link[b] = '\0';
-
-			return (link);
+	return (link);
 }
